055ce771c0afeda52939fa2836d05ff4d48abcfe
spre55 <spre55@mbp.local>
2022-04-21 21:17:50 +0900

    育成コマンド追加

--- components/NurturingField.vue
+++ components/NurturingField.vue
@@ -1,7 +1,16 @@
 <template>
-  <v-row justify="center" align="center">
+  <v-row class="ma-10" justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <canvas ref="canvas" width="300" height="500"></canvas>
+      <v-row justify="center" align="center">
+        <div class="love-gage">{{ love }}</div>
+      </v-row>
+      <v-row justify="center" align="center">
+        <canvas ref="canvas" width="300" height="500"></canvas>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-btn class="ma-4" v-on:click="state='happy'">エサやり</v-btn>
+        <v-btn class="ma-4" v-on:click="state='bokoboko'">ボコボコ</v-btn>
+      </v-row>
     </v-col>
   </v-row>
 </template>
@@ -16,13 +25,35 @@ export default {
   x: 0,
   y: 0,
   image: null,
+  happyImage: null,
+  bokobokoImage: null,
+  buffer: 0,
+  data() {
+    return {
+      love: 0,
+    }
+  },
+  watch: {
+    love: function (n, o) {
+      console.log(n, o);
+    },
+  },
   beforeCreate() {
     this.image = new Image();
     this.image.src = '/monster.png';
+
+    this.happyImage = new Image();
+    this.happyImage.src = '/k_happy.png';
+
+    this.bokobokoImage = new Image();
+    this.bokobokoImage.src = '/bokoboko.png';
+
     this.x = 100;
     this.y = 250;
     this.directionX = 1;
     this.directionY = 1;
+    this.buffer = 0;
+    this.love = 0;
   },
   mounted() {
     this.canvas = this.$refs.canvas;
@@ -32,16 +63,27 @@ export default {
   methods: {
     drawing() {
       let draw = () => {
-        if (Date.now() < (this.timestamp)) return requestAnimationFrame(draw);
-        this.context.clearRect(0,0,300,500);
-        this.context.drawImage(this.image, this.x, this.y, 100, 100);
+        if (Date.now() < (this.timestamp + this.buffer)) return requestAnimationFrame(draw);
+        this.context.clearRect(0,0,300,500); // 画面リフレッシュ
+        switch (this.state) {
+          case 'happy':
+            this.happy();
+            break;
+          case 'bokoboko':
+            this.bokoboko();
+            break;
+          default:
+            this.walk();
+            break;
+        }
         this.timestamp = Date.now();
-        this.walk();
         requestAnimationFrame(draw);
       }
       draw();
     },
     walk() {
+      this.context.drawImage(this.image, this.x, this.y, 100, 100);
+
       if (this.canvas.width < this.x + 100 || this.x <= 0) {
         this.directionX *= -1;
         if (Math.floor(Math.random() * 2) === 0) {
@@ -58,9 +100,22 @@ export default {
 
       this.x += (this.directionX * Math.floor(Math.random() * 5));
       this.y += (this.directionY * Math.floor(Math.random() * 5));
+      this.buffer = 0;
+    },
+    happy() {
+      this.context.drawImage(this.happyImage, this.x, this.y, 100, 100);
+      this.state = 'usually';
+      this.buffer = 1000;
+      this.love++;
+    },
+    bokoboko() {
+      this.context.drawImage(this.bokobokoImage, this.x, this.y, 100, 100);
+      this.state = 'usually';
+      this.buffer = 1000;
+      this.love++;
     },
     changeDirection() {
-
+      
     },
   },
 }
@@ -69,5 +124,16 @@ export default {
 <style scoped>
 canvas {
   border: 1px solid #000;
+}
+
+.love-gage {
+  background-image: url('/heart.png');
+  background-size: contain;
+  height: 50px;
+  width: 50px;
+  text-align: center;
+  margin: auto;
+  font-size: 15pt;
+  padding-top: 10px;
 }
 </style>
